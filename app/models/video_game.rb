4b36e09d7b2c84b9efc3c2fad0a683d28dc7fecb
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-05 09:41:17 -0400

    Initialize VideoGame model and migration and include relationships

---
+++ app/models/video_game.rb
@@ -0,0 +1,5 @@
+class VideoGame < ApplicationRecord
+  belongs_to :genre
+  has_many :play_sessions
+  has_many :players, through: :play_sessions
+end
---
+++ db/migrate/20190605133914_create_video_games.rb
@@ -0,0 +1,13 @@
+class CreateVideoGames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :video_games do |t|
+      t.string :name
+      t.string :description
+      t.integer :rating
+      t.integer :release_year
+      t.integer :genre_id
+
+      t.timestamps
+    end
+  end
+end
