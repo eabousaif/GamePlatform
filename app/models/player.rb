b26d54e31ccedf40c419927da7be627854a56919
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-05 10:08:09 -0400

    Install Devise

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 class Player < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :play_sessions
   has_many :video_games, through: :play_sessions
   validates :first_name, :last_name, :gamer_tag, :email, :password, presence: true
