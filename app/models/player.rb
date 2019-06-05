1210a02e6b40596e4c9043fd6b36e5fd8ba718e0
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-05 10:01:12 -0400

    Add player validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ApplicationRecord
   has_many :play_sessions
   has_many :video_games, through: :play_sessions
+  validates :first_name, :last_name, :gamer_tag, :email, :password, presence: true
+  validates :gamer_tag, :email, uniqueness: true
+  validates :password, length: { within: 8..20},
 end
