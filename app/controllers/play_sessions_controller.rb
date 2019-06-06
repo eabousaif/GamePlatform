e22e5b95c943737ee2a914c21b281f5d482817f1
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-06 09:51:38 -0400

    Clean up association

--- app/controllers/play_sessions_controller.rb
+++ app/controllers/play_sessions_controller.rb
@@ -3,5 +3,22 @@
 class PlaySessionsController < ApplicationController
   def index; end
 
-  def new; end
+  def new
+    @play_session = PlaySession.new
+  end
+
+  def create
+    @play_session = PlaySession.new(play_session_params)
+    if @play_session.save
+      redirect_to player_video_games_path(player)
+    else
+      render "new"
+    end
+  end
+
+private
+
+  def play_session_params
+    params.require(:play_session).permit(:video_game_id, :player_id, :duration)
+  end
 end
