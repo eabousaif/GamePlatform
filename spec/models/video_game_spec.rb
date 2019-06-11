e3c866e5195d3bebcd56da6539003819d9f34df6
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-11 10:19:04 -0400

    DRY specs

--- spec/models/video_game_spec.rb
+++ spec/models/video_game_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe VideoGame, type: :model do
-  it "is valid with a name, description, rating, year, and genre" do
-    genre = Genre.create(name: "cool", id: 1)
-    video_game = described_class.new(name: "Super Mario Bros", description: "fire game", rating: "9.2", release_year: "1994", genre_id: genre.id)
-    expect(video_game).to be_valid
+  it "has a valid factory - name, description, rating, year, and genre" do
+    genre = Genre.create(name: "action-acventure", id: 2)
+    expect(create(:video_game)).to be_valid
+  end
+
+  it "is not valid without a name" do
+    expect(build(:video_game, name: nil)).not_to be_valid
   end
 end
