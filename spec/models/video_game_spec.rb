749c09547c5435e684cc9e16ca2b0451f010a1dd
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-11 15:12:31 -0400

    Fix rspec bug

--- spec/models/video_game_spec.rb
+++ spec/models/video_game_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe VideoGame, type: :model dogit
+RSpec.describe VideoGame, type: :model do
   it "has a valid factory - name, description, rating, year, and genre" do
     genre = Genre.create(name: "action-acventure", id: 2)
     expect(create(:video_game)).to be_valid
