7e8162f87f904637f0b5b75b275007048231e8fc
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-11 10:19:22 -0400

    DRY specs

--- spec/models/video_game_spec.rb
+++ spec/models/video_game_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe VideoGame, type: :model do
+RSpec.describe VideoGame, type: :model dogit
   it "has a valid factory - name, description, rating, year, and genre" do
     genre = Genre.create(name: "action-acventure", id: 2)
     expect(create(:video_game)).to be_valid
