a1d375c8a02a9b00c018dd01a2004e2b3408bdb4
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-07-10 10:03:15 -0400

    Add class to videogames list to prevent default

--- app/assets/javascripts/video_games.js
+++ app/assets/javascripts/video_games.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-  $("#games").on("click", function(game){
+  $(".game_item").on("click", function(game){
     game.preventDefault()
     console.log(this)
-    $.get(this.href).success(function(response){
-      $(div.display_games).html(response)
-    })
   })
 })
