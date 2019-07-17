4c4bc5fb2093491bf2188f8688f62e3fbedb2578
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-07-17 14:00:55 -0400

    Finalize project and add show and hide function to play sessions link

--- app/assets/javascripts/player_listener.js
+++ app/assets/javascripts/player_listener.js
@@ -1,11 +1,20 @@
 $(function() {
   const $play = $(".view_play_sessions")
-  $play.on("click", function(e){
+  $play.off("click").on("click", function(e){
     e.preventDefault()
-    $.getJSON(this.href).done(function(response){
-      const playSessions = new PlaySessionFormatter(response)
-      playSessions.playSessionsTemplate()
-    })
+
+    const $ul = $("div.display_play_sessions")
+
+    if ($("div.display_play_sessions:visible").length == 0) {
+      $.getJSON(this.href).done(function(response){
+        const playSessions = new PlaySessionFormatter(response)
+        playSessions.playSessionsTemplate()
+        $ul.show()
+      })
+    }
+    else {
+      $ul.hide()
+    }
   })
 })
 
